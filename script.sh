4bc22d6b398756311b2647b38af824537f49f42f
luigizuccarelli <lzuccare@redhat.com>
2017-04-22 12:37:02 +0100

    Fine tuning script for argus

--- script.sh
+++ script.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
 # Replace with the name of your executable
-EXEC="simple-service"
+EXEC="go-simple-service"
 
 if [ "$1" = "start" ]
 then 
-    ./$EXEC &
+    echo -e "\nStarting service $2"
+    cd $2
+    ./$EXEC &>/dev/null &disown 
+    echo -e "Service $2 started"
 fi
 
 if [ "$1" = "stop" ]
@@ -13,8 +16,18 @@ then
     PID=$(ps -ef | grep $EXEC | grep -v grep | awk '{print $2}')
 
     if [[ "$OSTYPE" == "linux-gnu" ]]; then
-        kill -SIGTERM $PID
+        kill $PID
     elif [[ "$OSTYPE" == "darwin"* ]]; then
         kill $PID
     fi
+
+    echo -e "Service stopped"
+fi
+
+if [ "$1" = "build" ]
+then
+  echo -e "\nBuilding application $2"
+  cd $2
+  go build .
+  echo -e "Application $2 built"
 fi
